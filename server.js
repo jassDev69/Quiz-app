a625314eb567835e4cf1cdc059e4542db17a8116
jaskaran <developer.jaskaran@gmail.com>
2020-05-07 14:41:51 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const PORT = 3001
+
+const db = require('./database/database')
+
+// body parser (will allow to post the data in body for api's)
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(cors())
+
+// api to get all the tracks
+app.get('/api/tracks',db.getAllTracks);
+
+// api to delete tracks
+app.delete('/api/tracks/:id',db.deleteTrack);
+
+//api to insert data in track table
+app.post('/api/tracks/',db.insertToFav);
+
+
+app.listen(PORT, () => console.log(`Hosted on server : ${PORT}`))
